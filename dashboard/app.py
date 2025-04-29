856f9b3f457c0f1277631b1ddc8f9c68e661dc1d
Muhammad Anser Sohaib <130187355+Anser2@users.noreply.github.com>
2025-04-30 00:52:03 +0500

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -452,7 +452,7 @@ with col_title:
 # ----------------------------
 @st.cache_resource
 def load_models():
-    vel_model, device = load_model("models/terraflow_v3_19M.pth")
+    vel_model, device = load_model("models/terraflow_transformer_model.pth")
     temp_model, temp_scaler = load_tempflow("models/best_tempflow_model.keras", "models/scaler_tempflow.save")
     return vel_model, device, temp_model, temp_scaler
 
